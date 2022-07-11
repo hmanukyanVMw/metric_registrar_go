7b2982c89376c2efdad8b35710980f85193ad9e8
hmanukyan <hmanukyan@vmware.com>
2022-07-11 15:00:34 +0400

    Make chanegs

--- cfPushSpaceApp.sh
+++ cfPushSpaceApp.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
+export SYSTEM_DOMAIN=sys.paradise.cf-app.com
+export CF_USERNAME=admin
+export CF_PASSWORD=h8-PSu67YSDBTJzmhjBbDOqsm1Jgl4Cp
 
-#cf target -s system
-#cd /Users/hmanukyan/workspace/metric_registrar_go;
+
+cf login -a api.${SYSTEM_DOMAIN} -u ${CF_USERNAME} -p ${CF_PASSWORD} -o system -s system
+cf create-org system
+cf target -o "system"
+cf create-space system
+cf target -o "system" -s "system"
 
 export name="system"
-#cf create-org ${name}
 cf target -o ${name}
 
 export time=$(date +"%T")
 
-for l in {1..20};  do {
+for l in {1..20}; do {
   export spaceName="paralel-$time"
   cf create-space ${spaceName}
   cf target -o ${name} -s ${spaceName}
 
-  for i in {1..5}; do {
-    export appName=$spaceName${l}${i}
+  for i in {1..3}; do {
+  export appName=$spaceName${l}${i}
 
-    cf push ${appName} -k 128M -m 128M
-    cf register-metrics-endpoint ${appName} /metrics --internal-port 8080
-    echo "register ${i}"
-  }; done
-
-  sleep 10
-  yes Y | cf delete-space ${spaceName}
+  cf push ${appName} -k 128M -m 128M
+  cf register-metrics-endpoint ${appName} /metrics --internal-port 8080
+  echo "register ${i}"
 }; done
 
-#for i in {1..1}; do {
-#     cf stop custom-metrics-${i}
-#
-##  cf scale custom-metrics-${i} -m 256 -k 64M
-## cf push custom-metrics-${i} -k 64M -m 64M
-##   echo "done ${i}"
-##
-## cf register-metrics-endpoint custom-metrics-${i} /metrics --internal-port 8080
-#  echo "register ${i}"
-#}; done
-
+  sleep 15
+  yes Y | cf delete-space ${spaceName}
+  sleep 10
+}; done
 
 
